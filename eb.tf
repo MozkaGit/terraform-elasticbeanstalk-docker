873ee73eace389770f37c53586f85b651e2640d7
MozkaGit <momodiaby@hotmail.fr>
2023-08-06 21:58:08 +0200

    Add version_label to env resources & split code to multiples files

--- eb.tf
+++ eb.tf
@@ -1,20 +1,11 @@
-# Define EB application code to upload
-resource "aws_s3_bucket" "game-2048" {
-  bucket = "2048-${random_pet.petname.id}"
-}
-
-resource "aws_s3_object" "game-2048" {
-  bucket = aws_s3_bucket.game-2048.id
-  key    = "beanstalk/Dockerrun.aws.json"
-  source = "Dockerrun.aws.json"
-}
-
-# Define EB application
+# Create the EB application
 resource "aws_elastic_beanstalk_application" "game-2048" {
   name        = "2048-game"
   description = "Elastic Beanstlak app for 2048"
 }
 
+# Create an EB application version associated with the application "2048-game"
+# Use the S3 bucket and S3 object to fetch the application source code
 resource "aws_elastic_beanstalk_application_version" "game-2048" {
   name        = "2048-version"
   application = "2048-game"
@@ -23,27 +14,17 @@ resource "aws_elastic_beanstalk_application_version" "game-2048" {
   key         = aws_s3_object.game-2048.id
 }
 
-# Define EB environment
+# Create an EB environment linked to the application "2048-game"
+# Use the application version "2048-version" for deployment
 resource "aws_elastic_beanstalk_environment" "env-2048" {
   name                = "2048-game"
   application         = aws_elastic_beanstalk_application.game-2048.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.0.0 running Docker"
+  version_label       = aws_elastic_beanstalk_application_version.game-2048.name
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name = "IamInstanceProfile"
     value = "aws-elasticbeanstalk-ec2-role"
   }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name = "ServiceRole"
-    value = "aws-elasticbeanstalk-ec2-role"
-  }
-
-}
-
-# To print the env CNAME after creation
-output "env_cname" {
-  value = "http://${aws_elastic_beanstalk_environment.env-2048.cname}"
 }
